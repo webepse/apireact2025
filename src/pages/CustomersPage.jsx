dfc63196df883611f79879bcef7ffc3d7b0bd2d2
berti <berti@CLASSES.LOCAL>
2025-01-28 11:23:05 +0100

    axios v2

--- src/pages/CustomersPage.jsx
+++ src/pages/CustomersPage.jsx
@@ -14,7 +14,41 @@ const CustomersPage = (props) => {
  
     return ( 
         <>
-
+            <h1>Liste des clients</h1>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Client</th>
+                        <th>Email</th>
+                        <th>Entreprise</th>
+                        <th>Factures</th>
+                        <th className="text-center">Montant total</th>
+                        <th className="text-center">Montant Restant</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {customers.map(customer => (
+                        <tr key={customer.id}>
+                            <td>{customer.id}</td>
+                            <td>{customer.firstName} {customer.lastName}</td>
+                            <td>{customer.email}</td>
+                            <td>{customer.company}</td>
+                            <td className="text-center">
+                                <span className="badge text-bg-primary">
+                                    {customer.invoices.length}
+                                </span>
+                            </td>
+                            <td className="text-center">{customer.totalAmount.toLocaleString()}</td>
+                            <td className="text-center">{customer.unpaidAmount.toLocaleString()}</td>
+                            <td>
+                                <button className="btn btn-sm btn-danger">Supprimer</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
      );
 }
