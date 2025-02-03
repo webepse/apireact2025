ca6240cbfa99441d5fbae7fe0eac4a906aaba819
berti <berti@CLASSES.LOCAL>
2025-02-03 11:38:40 +0100

    invoices with search & pagination

--- src/pages/InvoicesPage.jsx
+++ src/pages/InvoicesPage.jsx
@@ -20,7 +20,7 @@ const InvoicesPage = (props) => {
     const [invoices, setInvoices] = useState([])
     const [currentPage, setCurrentPage] = useState(1)
     const [search, setSearch] = useState("")
-    const itemsPerPage = 10
+    const itemsPerPage = 30
 
     // recup les invoices
     const fetchInvoice = async () => {
@@ -38,14 +38,34 @@ const InvoicesPage = (props) => {
         fetchInvoice()
     },[])
 
+    const handlePageChange = (page) => {
+        setCurrentPage(page)
+    }
+
+    const handleSearch = event => {
+        const value = event.currentTarget.value
+        setSearch(value)
+        // remettre la pagination à 1 après la recherche
+        setCurrentPage(1)
+    }
+
     const formatDate = (str) => moment(str).format("DD/MM/YYYY")
+
+    const filteredInvoices = invoices.filter(i =>
+        i.customer.lastName.toLowerCase().includes(search.toLowerCase()) ||
+        i.customer.firstName.toLowerCase().includes(search.toLowerCase()) ||
+        i.amount.toString().includes(search.toLowerCase()) ||
+        STATUS_LABELS[i.status].toLowerCase().includes(search.toLowerCase())
+    )
+
+    const paginatedInvoices = Pagination.getData(filteredInvoices, currentPage, itemsPerPage)
 
     return ( 
         <>
             <h1>Liste des factures</h1>
             {/* filtre */}
             <div className="form-group my-3">
-                <input type="text" className="form-control" placeholder="Rechercher..." />
+                <input type="text" className="form-control" placeholder="Rechercher..." onChange={handleSearch} value={search} />
             </div>
             <table className="table table-hover">
                 <thead>
@@ -60,7 +80,7 @@ const InvoicesPage = (props) => {
                 </thead>
                 <tbody>
                     {
-                        invoices.map(invoice => (
+                        paginatedInvoices.map(invoice => (
                             <tr key={invoice.id}>
                                 <td className="text-center">{invoice.id}</td>
                                 <td className="text-center">{invoice.customer.firstName} {invoice.customer.lastName}</td>
@@ -84,6 +104,12 @@ const InvoicesPage = (props) => {
                     }
                 </tbody>
             </table>
+            <Pagination 
+                currentPage={currentPage}
+                itemsPerPage={itemsPerPage}
+                length={filteredInvoices.length}
+                onPageChanged={handlePageChange}
+            />
         </>
      );
 }
