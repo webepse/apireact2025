1264f2cd9c4141c08da33792e11129d48bad10e1
Boderio <boderiotrigan@hotmail.com>
2025-02-04 11:46:38 +0100

    Authentification with context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import authAPI from "../services/authAPI";
+import { useContext } from "react";
+import AuthContext from "../contexts/AuthContext";
 
 const Navbar = (props) => {
 
     const navigate = useNavigate()
+    const {isAuthenticated,setIsAuthenticated} = useContext(AuthContext)
 
     const handleLogout = () => {
         authAPI.logout()
-        props.onLogout(false)
+        setIsAuthenticated(false)
         navigate('/login', {replace:true})
     }
 
@@ -29,7 +32,7 @@ const Navbar = (props) => {
                         </li>
                     </ul>
                     <ul className="navbar-nav ms-auto">
-                        {(!props.isAuthenticated) ? (
+                        {(!isAuthenticated) ? (
                             <>
                                 <li className="nav-item">
                                     <a href="#" className="nav-link">Inscription</a>
